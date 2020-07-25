0f1453d27d332c9722788fe1f461fd09a928d672
Adrisson Samersla <adrissonsamersla@gmail.com>
2020-07-24 21:33:46 -0300

    Adjusting names in DAT.GUI

--- loader.js
+++ loader.js
@@ -1,17 +1,17 @@
 var earthHubbleDist = 568; // km
 var hubbleData = {
-    orbitRate: 95.47/(24*60), // dia
+    orbitRate: 95.47 / (24 * 60), // dia
     distanceFromAxis: earthHubbleDist + earthRadius,
     name: "hubble",
 }
 var earthISSDist = 408; // km
 var ISSData = {
-    orbitRate: 91.34/(24*60), // min
+    orbitRate: 91.34 / (24 * 60), // min
     distanceFromAxis: earthISSDist + earthRadius,
     name: "iss",
 }
 var sat;
-var satData = { size: 1};
+var satData = { size: 1 };
 orbitData.value = 20000
 
 const sceneBuilder = (modelName) => {
@@ -20,7 +20,7 @@ const sceneBuilder = (modelName) => {
         75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(10, 10, 10);
 
-    if (modelName == "Hubble.glb" || modelName ==  "ISS_stationary.glb"){
+    if (modelName == "Hubble.glb" || modelName == "ISS_stationary.glb") {
         camera = new THREE.PerspectiveCamera(
             70, // field of view
             window.innerWidth / window.innerHeight, // aspect ratio
@@ -89,77 +89,77 @@ const sceneBuilder = (modelName) => {
         [-1, 1, 0],
         [0, 1, 1],
 
-    [1, 0, 0],
-    [0, 0, -1],
-    [-1, 0, 0],
-    [0, 0, 1],
+        [1, 0, 0],
+        [0, 0, -1],
+        [-1, 0, 0],
+        [0, 0, 1],
 
-    [1, -1, 0],
-    [0, -1, -1],
-    [-1, -1, 0],
-    [0, -1, 1],
-  ];
-  directionalLights.forEach((dl) => {
-    const light = new THREE.DirectionalLight(0xffffff, 0.35);
+        [1, -1, 0],
+        [0, -1, -1],
+        [-1, -1, 0],
+        [0, -1, 1],
+    ];
+    directionalLights.forEach((dl) => {
+        const light = new THREE.DirectionalLight(0xffffff, 0.35);
 
         light.position.set(10 * dl[0], 10 * dl[1], 10 * dl[2]);
         light.lookAt(new THREE.Vector3(0, 0, 0));
 
         scene.add(light)
     });
-    
+
     // Create the Earth, the Moon, and a ring around the earth.
-    if (modelName == "Hubble.glb" || modelName ==  "ISS_stationary.glb"){
+    if (modelName == "Hubble.glb" || modelName == "ISS_stationary.glb") {
         earthData = constructPlanetData(365.2564, 0.0015, 0, "earth", "img/earth.jpg", earthRadius, planetSegments);
         earth = loadTexturedPlanet(earthData, 0, 0, 0);
-        if (modelName == "Hubble.glb" )
+        if (modelName == "Hubble.glb")
             satRing = getTube(hubbleData.distanceFromAxis, 1, 480, 0xffffff, "ring", earthData.distanceFromAxis);
         else satRing = getTube(ISSData.distanceFromAxis, 1, 480, 0xffffff, "ring", earthData.distanceFromAxis);
     }
 
     // Add the satelite
     modelLoader(modelName, scene);
-    
+
     // Create the GUI that displays controls.
     var gui = new dat.GUI();
     var obj = {};
-    obj.SolarSystem = function() {
+    obj.SolarSystem = function () {
         window.open("/")
     }
-    var folder2 = gui.addFolder('Speed');
+    var folder2 = gui.addFolder('Simulation');
     folder2.add(orbitData, 'value', 20000, 50000, 0.1);
     folder2.add(orbitData, 'runOrbit', 0, 1);
     folder2.add(orbitData, 'runRotation', 0, 1);
-    gui.add(obj, 'SolarSystem');
+    gui.add(obj, 'SolarSystem').name('Solar System');
     var folder = gui.addFolder('Spacecraft');
     folder.open();
     var modelSize;
     if (modelName != "Hubble.glb")
-        obj.Hubble = function() {
+        obj.Hubble = function () {
             window.open("hubble.html");
         }
     else modelSize = 'Hubble';
-    if (modelName != "Cassini.glb") 
-        obj.Cassini = function() {
+    if (modelName != "Cassini.glb")
+        obj.Cassini = function () {
             window.open("cassini.html");
         }
     else modelSize = 'Cassini';
-    if (modelName != "ISS_stationary.glb") 
-        obj.ISS = function() {
+    if (modelName != "ISS_stationary.glb")
+        obj.ISS = function () {
             window.open("iss.html");
         }
     else modelSize = 'ISS';
     if (modelName != "New_Horizons.glb")
-        obj.NewHorizons = function() {
+        obj.NewHorizons = function () {
             window.open("newHorizons.html");
         }
     else modelSize = 'NewHorizons';
     if (modelName != "Voyager.glb")
-        obj.Voyager = function() {
+        obj.Voyager = function () {
             window.open("voyager.html");
         }
     else modelSize = 'Voyager';
-    if (typeof modelSize != 'undefined'){
+    if (typeof modelSize != 'undefined') {
         var folder3 = gui.addFolder(modelSize);
         folder3.open();
         folder3.add(satData, 'size', 1, 10, 0.1);
@@ -167,7 +167,7 @@ const sceneBuilder = (modelName) => {
     if (modelName != "Hubble.glb") folder.add(obj, 'Hubble');
     if (modelName != "Cassini.glb") folder.add(obj, 'Cassini');
     if (modelName != "ISS_stationary.glb") folder.add(obj, 'ISS');
-    if (modelName != "New_Horizons.glb") folder.add(obj, 'NewHorizons');
+    if (modelName != "New_Horizons.glb") folder.add(obj, 'NewHorizons').name('New Horizons');
     if (modelName != "Voyager.glb") folder.add(obj, 'Voyager');
 
     const animate = () => {
@@ -179,22 +179,22 @@ const sceneBuilder = (modelName) => {
         renderer.render(scene, camera);
         controls.update();
 
-        if (typeof sat != "undefined"){
-            sat.scale.set(satData.size,satData.size,satData.size);
-            if (modelName == "Hubble.glb" || modelName ==  "ISS_stationary.glb"){
+        if (typeof sat != "undefined") {
+            sat.scale.set(satData.size, satData.size, satData.size);
+            if (modelName == "Hubble.glb" || modelName == "ISS_stationary.glb") {
                 movePlanet(earth, earthData, time, false, "rot"); // only rotation
-                if (modelName == "Hubble.glb") 
+                if (modelName == "Hubble.glb")
                     moveSat(sat, earth, hubbleData, time, "sat");
-                if (modelName == "ISS_stationary.glb") 
+                if (modelName == "ISS_stationary.glb")
                     moveSat(sat, earth, ISSData, time, "sat");
             }
         }
 
         requestAnimationFrame(animate);
-        
+
     }
 
-  animate();
+    animate();
 };
 
 const modelLoader = (modelName, scene) => {
@@ -204,13 +204,13 @@ const modelLoader = (modelName, scene) => {
         (gltf) => {
             sat = gltf.scene;
             scene.add(sat);
-            if (modelName == "Hubble.glb"){
+            if (modelName == "Hubble.glb") {
                 sat.position.set(hubbleData.distanceFromAxis, 0, 0);
                 satData.size = 10;
             }
             if (modelName == "ISS_stationary.glb")
                 sat.position.set(ISSData.distanceFromAxis, 0, 0);
-        
+
             console.log("Spacecraft added!");
         },
         (xhr) => console.log(`${xhr.loaded / xhr.total * 100} % loaded...`),
